6e352daf34158530104b90c483d6e3188984bc33
aleksgurbanov <aliskandarvg@code.edu.az>
2022-02-12 21:55:48 +0400

    csharp methods

---
+++ task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zehmet olmazsa eded daxil edin");
+            string reqem = Console.ReadLine();
+            int num = int.Parse(reqem);
+            Console.WriteLine(isprime(num));
+        }
+
+        static bool isprime(int a)
+        {
+            int netice = 0;
+            int i = 0;
+            
+            for (i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    netice++;
+                }
+
+
+
+
+
+
+            }
+            if (netice == 2)
+                return true;
+            return false;
+
+        }
+
+
+            
+
+
+
+     }
+ }
---
+++ task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task3new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double exam1 = 0, exam2 = 0;
+            double average = (exam1 + exam2) / 2;
+            
+            Console.Write("birinci imtahani daxil edin:");
+        firststep:
+
+
+            exam1 = Convert.ToDouble(Console.ReadLine());
+         secondstep:
+                Console.Write("ikinci imtahan neticesini daxil edin:");
+        
+            exam2 = Convert.ToDouble(Console.ReadLine());
+            calcavg(average);
+        }
+        static void calcavg (double average )
+        { if(average >= 60)
+            {
+                Console.WriteLine("mezun oldunuz");
+
+            }
+            else
+            {
+                Console.WriteLine("kesildiniz");
+            }
+
+
+
+        }
+    }
+}
